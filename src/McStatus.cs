78e1988fb670df8e2e20c97f45bc80a6d52e1621
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-25 04:22:35 +0300

    Update and rename mc_status.sh to McStatus.cs

---
+++ src/McStatus.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace MinecraftStatusApi {
+    public class McStatusApi {
+        private readonly HttpClient httpClient;
+        private readonly string apiUrl = "https://api.mcstatus.io/v2";
+        public McStatusApi() {
+            httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
+                "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
+            httpClient.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<string> GetJavaServerStatus(string address) {
+            var response = await httpClient.GetAsync($"{apiUrl}/status/java/{address}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetBedrockServerStatus(string address) {
+            var response = await httpClient.GetAsync($"{apiUrl}/status/bedrock/{address}");
+            response.EnsureSuccessStatusCode();
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
