ed74387d5db5e7cf146f68d409772554d300ca83
huok <77536370+dee-shar@users.noreply.github.com>
2025-10-26 01:54:35 +0300

    Update McStatus.cs

--- src/McStatus.cs
+++ src/McStatus.cs
@@ -1,13 +1,15 @@
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Threading.Tasks;
 
-namespace MinecraftStatusApi {
-    public class McStatusApi {
+namespace MinecraftStatusApi
+{
+    public class McStatusApi
+    {
         private readonly HttpClient httpClient;
         private readonly string apiUrl = "https://api.mcstatus.io/v2";
-        public McStatusApi() {
+        public McStatusApi()
+        {
             httpClient = new HttpClient();
             httpClient.DefaultRequestHeaders.UserAgent.ParseAdd(
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36");
@@ -15,15 +17,15 @@ namespace MinecraftStatusApi {
                 new MediaTypeWithQualityHeaderValue("application/json"));
         }
 
-        public async Task<string> GetJavaServerStatus(string address) {
+        public async Task<string> GetJavaServerStatus(string address)
+        {
             var response = await httpClient.GetAsync($"{apiUrl}/status/java/{address}");
-            response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
         }
 
-        public async Task<string> GetBedrockServerStatus(string address) {
+        public async Task<string> GetBedrockServerStatus(string address)
+        {
             var response = await httpClient.GetAsync($"{apiUrl}/status/bedrock/{address}");
-            response.EnsureSuccessStatusCode();
             return await response.Content.ReadAsStringAsync();
         }
     }
